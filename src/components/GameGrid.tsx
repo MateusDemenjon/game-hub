d78788be524be9fb9869331ff6d6af2495ad298a
Mateus <mateusdemenjon@gmail.com>
2024-09-05 13:43:12 -0300

    B1.2v

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,20 +1,35 @@
 import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
+import GamerCardSkeleton from './GamerCardSkeleton';
+import GameCardContainer from './GameCardContainer';
+import { Genre } from '../hooks/useGenres';
 
-const GameGrid = () => {
-    const { games, error } = useGames();
+interface Props {
+    selectedGenre: Genre | null
+}
+
+const GameGrid = ({ selectedGenre }: Props) => {
+    const { data, error, isLoading } = useGames(selectedGenre);
+    const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     return (
         <>
             {error && <Text>{error} </Text>}
-            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding={'10px'} spacing={10}>
-                {games.map((game) => (
-                    <GameCard key={game.id} game={game} />
+            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding='10px' spacing={3}>
+                {isLoading && skeletons.map(skeleton => (
+                    <GameCardContainer key={skeleton}>
+                        <GamerCardSkeleton />
+                    </GameCardContainer>
+                ))}
+                {data.map((game) => (
+                    <GameCardContainer key={game.id}>
+                        <GameCard game={game} />
+                    </GameCardContainer>
                 ))}
             </SimpleGrid>
         </>
     )
 }
 
-export default GameGrid
+export default GameGrid;
