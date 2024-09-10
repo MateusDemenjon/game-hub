365962cddfc3ad75c519b1668a1783ad4a8b9565
Mateus <mateusdemenjon@gmail.com>
2024-09-10 12:11:43 -0300

    F1.5v

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ import { Genre } from "./hooks/useGenres"
 import PlatformSelector from "./components/PlatformSelector"
 import { Platform } from "./hooks/useGames"
 import SortSelector from "./components/SortSelector"
+import GameHeading from "./components/GameHeading"
 
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 
 function App() {
@@ -26,7 +28,7 @@ function App() {
       lg: '280px 1fr'
     }}>
     <GridItem area='nav'>
-      <NavBar />
+      <NavBar onSearch={(searchText) => setGameQuery({ ...GameQuery, searchText })} />
     </GridItem>
     <Show above="lg">
       <GridItem area='aside' paddingX={5}>
@@ -34,15 +36,18 @@ function App() {
       </GridItem>
     </Show>
     <GridItem area='main'>
-      <Flex paddingLeft={2} marginBottom={5}>
-        <Box marginRight={5}>
-          <PlatformSelector selectedPlatform={GameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...GameQuery, platform })} />
-        </Box>
-        <SortSelector sortOrder={GameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({ ...GameQuery, sortOrder })} />
-      </Flex>
+      <Box paddingLeft={2}>
+        <GameHeading gameQuery={GameQuery} />
+        <Flex marginBottom={5}>
+          <Box marginRight={5}>
+            <PlatformSelector selectedPlatform={GameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...GameQuery, platform })} />
+          </Box>
+          <SortSelector sortOrder={GameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({ ...GameQuery, sortOrder })} />
+        </Flex>
+      </Box>
       <GameGrid gameQuery={GameQuery} />
     </GridItem>
-  </Grid>
+  </Grid >
 }
 
 export default App
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,24 +1,27 @@
 import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
+import { Box, Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
 import PlatformIconList from './PlatformIconList'
 import CriticScore from './CriticScore'
 import getCroppedImageUrl from '../services/image-url'
+import Emoji from './Emoji'
 
 interface Props {
     game: Game
 }
 const GameCard = ({ game }: Props) => {
     return (
-        <Card width='300px' borderRadius={10} overflow='hidden'>
-            <Image src={getCroppedImageUrl(game.background_image)} />
-            <CardBody>
-                <Heading fontSize={'2xl'}>{game.name}</Heading>
-                <HStack justifyContent='space-between'>
-                    <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}></PlatformIconList>
-                    <CriticScore score={game.metacritic} />
-                </HStack>
-            </CardBody>
-        </Card>
+        <Box display="flex" justifyContent="center">
+            <Card width='300px' borderRadius={10} overflow='hidden'>
+                <Image src={getCroppedImageUrl(game.background_image)} />
+                <CardBody>
+                    <HStack justifyContent='space-between' marginBottom={3}>
+                        <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}></PlatformIconList>
+                        <CriticScore score={game.metacritic} />
+                    </HStack>
+                    <Heading fontSize={'2xl'}>{game.name}<Emoji rating={game.rating_top} /></Heading>
+                </CardBody>
+            </Card>
+        </Box>
     )
 }
 
